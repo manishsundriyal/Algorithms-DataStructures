84f80aa04bb505bd6e44a451f1befd0f3b919285
manish <msundriyal27@gmail.com>
2017-09-13 17:03:54 +0530

    TOH added

---
+++ TowerOfHanoi.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+void TowerOfHanoi(int No_of_dics,char source,char dest,char help){
+
+	if(No_of_dics==0) return;
+	TowerOfHanoi(No_of_dics-1,source,help,dest);
+	cout<<"Move disc from "<<source<<" to "<<dest<<endl;
+	TowerOfHanoi(No_of_dics-1,help,dest,source);
+}
+int main(){
+	int No_of_dics;
+	cin>>No_of_dics;
+	char source='S';
+	//source stack
+	char dest='D';
+	//destinataion
+	char help='H';
+	//helper
+	TowerOfHanoi(No_of_dics,source,dest,help);	
+	return 0;
+}
